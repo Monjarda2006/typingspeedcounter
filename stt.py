4569519ca1b10b494f5f77c6532a970088e30015
Jaime Roque Silveira Monjardino <monjardinojaime28@gmail.com>
2020-10-10 22:04:27 +0000

    Optimized and fixed bugs

--- stt.py
+++ stt.py
@@ -8,58 +8,150 @@ words = {
 3 : "about",
 4 : "above",
 5 : "accept",
-6 : "according"
-
+6 : "according",
+7 : "account",
+8 : "alone",
+9 : "agency",
+10 : "arm",
+11 : "animal",
+12 : "air",
+13 : "after",
+14 : "anyone",
+15 : "another",
+16 : "apply",
+17 : "answer",
+18 : "attention",
+19 : "around",
+20 : "area",
+21 : "article",
+22 : "available",
+23 : "baby",
+24 : "back",
+25 : "bad",
+26 : "bag",
+27 : "ball",
+28 : "bank",
+29 : "bar",
+30 : "base",
+31 : "beat",
+32 : "beautiful",
+33 : "become",
+34 : "bed",
+35 : "before",
+36 : "begin",
+37 : "between",
+38 : "blood",
+39 : "bilion",
+40 : "buy",
+41 : "box",
+42 : "build",
+43 : "bug",
+44 : "call",
+45 : "camera",
+46 : "campaign",
+47 : "cancer",
+48 : "capital",
+49 : "card",
+50 : "car",
+51 : "carrer",
+52 : "chair",
+53 : "center",
+54 : "catch",
+55 : "close",
+56 : "city",
+57 : "class",
+58 : "company",
+59 : "cup",
+60 : "control",
+61 : "dark",
+62 : "day",
+63 : "decide",
+64 : "death",
+65 : "develop",
+66 : "detail",
 }
 
 
 app = "Typing Test"
 appcenter = app.center(100)
-correct = 0
-fail = 0
 
-def type():
-    print("ola")
+
+# Globais
+
+actual_word = ""
+corrects = 0
+fails = 0
+
+
+# Configs
+
+typing_time = 60
+
+
+def checkWord(word, typed_word):
+
+    if word.strip().lower() == typed_word.strip().lower():
+        print("-=-" * 30)
+        correct()
+    else:
+        print("-=-" * 30)
+        fail()
+
+
+def correct():
+    global corrects
+    corrects += 1
+
+def fail():
+    global fails
+    fails += 1
+
+
+def genNewWord():
+    randomword = randint(1, 66)
+    return words[randomword]
+
+    # typedword = input("")
 
 print("-=-"*30)
 print(appcenter)
 print("-=-"*30)
 
-def correct():
-    correct += 1
-    print("-=-"*30)
-    randomword  = randint(1, 20)
-    print(words[randomword])
-    typedword = input("")
 
-def fail():
-    fail += 1
-    print("-=-"*30)
-    randomword  = randint(1, 20)
-    print(words[randomword])
-    typedword = input("")
 
 start = str(input("Press (S) to Start ")).upper()
 
-while start in "SsNn":
-    if start in "Ss":
-        print("3")
-        time.sleep(1)
-        print("2")
-        time.sleep(1)
-        print("1")
-        time.sleep(1)
-        type = print("Start")
-        now = time.gmtime(0)
-        end = time.gmtime(60)
-        while now != end:
-            if now != end:
-                randomword  = randint(1, 20)
-                print(words[randomword])
-                typedword = input("")
-                if typedword == words:
-                    correct()
-                else:
-                    fail()
-            else:
-                break
+
+if start in "Ss":
+
+
+    print("3")
+    time.sleep(1)
+
+    print("2")
+    time.sleep(1)
+
+    print("1")
+    time.sleep(1)
+    print("Start")
+
+
+    end_time = time.time() + typing_time
+
+    while time.time() < end_time:
+
+
+        actual_word = genNewWord()
+        print(actual_word)
+        typed_word = input()
+
+
+        checkWord(actual_word, typed_word)
+
+
+    print("Done")
+    print(f"{corrects} WPM")
+    print(f"Failed {fails} words ")
+
+else:
+    print("Quiting...")
